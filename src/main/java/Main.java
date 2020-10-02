f905d4e6400db1ec2b4c2fb208ab3050a238bb98
Anatoly <itrm000@gmail.com>
2020-10-02 13:27:43 +0500

    fixed #1 - исправлена ошибка неиспользуемой переменной bonus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,8 @@ public class Main {
         
         long amount = 1000_60;
         boolean registered = true;
-        
-        long bonus = service.calculate(amount, registered);
 
+        long bonus = service.calculate(amount, registered);
         System.out.println(bonus);
     }
 }
